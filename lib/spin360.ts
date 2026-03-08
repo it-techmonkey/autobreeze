56f1436323289668986c1cfc831a30ff2a1717ca
= <=>
2026-03-08 12:18:10 +0530

    Changes in cars

--- lib/spin360.ts
+++ lib/spin360.ts
@@ -42,7 +42,7 @@ const CAR_SPIN_360_BY_TITLE: Record<string, string> = {
     "https://spins.impel.io/dubizzlenonturntable/ms_autobreez_infinity_qx50",
   "infinity qx 50":
     "https://spins.impel.io/dubizzlenonturntable/ms_autobreez_infinity_qx50",
-  "mazda cx 30": "https://spins.impel.io/dubizzlenonturntable/mz_autobreez_mazda_cx30",
+  "mazda cx 30": "https://spins.impel.io/dubizzlenonturntable/ms_autobreez_mazda_cx30",
   "mazda cx5": "https://spins.impel.io/dubizzlenonturntable/ms_autobrees_cx5",
   jeep: "https://spins.impel.io/dubizzlenonturntable/ms_autobreez_jeep",
   "bmw x5": "https://spins.impel.io/dubizzlenonturntable/ms_autobreez_bmwx5",
